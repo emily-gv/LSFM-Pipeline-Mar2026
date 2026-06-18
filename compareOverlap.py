80f4a28f5d37a65e2d1559acdc6190793b6da7bb
emily-gv <emilygarciavolk@gmail.com>
2026-06-18 14:54:44 -0600

    Add function to shift pixels in mask

--- compareOverlap.py
+++ compareOverlap.py
@@ -2,6 +2,8 @@ from TissueSegmentation.data_loader import get_images_from_path
 import cv2
 import os 
 import numpy as np
+from PIL import Image
+from scipy import ndimage
 
 def compute_image_overlay(image1, image2, output_folder, str_description):
     mask1 = cv2.imread(image1, cv2.IMREAD_GRAYSCALE) == 255  
@@ -18,8 +20,9 @@ def compute_image_overlay(image1, image2, output_folder, str_description):
         output[~mask1 & mask2] = [0,255,0] # green
         base = os.path.splitext(os.path.basename(image1))[0]
         output_path = os.path.join(output_folder, base + '_' + str_description + '_merged.png')
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
         cv2.imwrite(output_path, output)
-
 
 def compute_image_overlay_folder(folder_slices_1, folder_slices_2, output_folder, str_description):
     list_slices_1 = get_images_from_path(folder_slices_1)
@@ -35,15 +38,52 @@ def compute_image_overlay_folder(folder_slices_1, folder_slices_2, output_folder
         img_slice_path_1 = list_slices_1[i]
         img_slice_path_2 = list_slices_2[i]
         compute_image_overlay(img_slice_path_1, img_slice_path_2, output_folder, str_description)
-    
+
+# https://www.w3schools.com/python/numpy/numpy_array_slicing.asp
+# Shift up = array[:-x, :]
+# Shift down = array[:x, ]
+# Shift left = array[:,:-x]
+# Shift right = array[;,:x]
+def correct_shift(folder_slices_toShift, output_folder, shift_value):
+    # output = np.zeros_like(image)
+    # output[:-shift_value, :] = image[shift_value:, :]
+    list_slices = get_images_from_path(folder_slices_toShift)
+    n_list_slices = len(list_slices)
+    for i in range(n_list_slices):
+        img_slice_path = list_slices[i]
+        img_original = cv2.imread(img_slice_path, cv2.IMREAD_GRAYSCALE) == 255
+        
+        img_shifted = np.zeros_like(img_original)
+        # THE BELOW SHIFTS IMAGE UP
+        img_shifted[:-shift_value,:] = img_original[shift_value:, :]
+        img_shifted_bw = (img_shifted.astype(np.uint8)) * 255
+
+        base = os.path.splitext(os.path.basename(img_slice_path))[0]
+        output_path = os.path.join(output_folder, base + '_shifted.png')
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+        
+        cv2.imwrite(output_path, img_shifted_bw)
+
+    # y, x = np.where(image == 1)
+    # new_y = y - shift_value
+    # valid = new_y >= 0
+    # output[new_y[valid], x[valid]] = image[valid]
+
 def main():
     # image1 = '/home/emilygv/Desktop/CC3_pHH3_comparison/Aug28_2025_27/Step25b_pHH3_slices_binary/Aug28_2025_27_slice_0350.png'
     # image2 = '/home/emilygv/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_slices_binary/Aug28_2025_27_slice_0350.png' 
-    folder_slices_1 = '/home/emilygv/Desktop/CC3_pHH3_comparison/Aug28_2025_27/Step25b_pHH3_slices_binary'
-    folder_slices_2 = '/home/emilygv/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_slices_binary' 
-    output_folder = '/home/emilygv/Desktop/CC3_pHH3_comparison'
-    str_description = '_phh3_cc3'
     # compute_image_overlay(image1, image2, output_folder, str_description)
+
+
+    # folder_slices_toShift = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_slices_binary'
+    # output_folder = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_slices_binary_shifted'
+    # correct_shift(folder_slices_toShift, output_folder, 12)
+    
+    folder_slices_1 = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/Step25b_pHH3_slices_binary'
+    folder_slices_2 = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_slices_binary_shifted' 
+    output_folder = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/CC3_pHH3_comparison_shifted'
+    str_description = 'phh3_cc3'
     compute_image_overlay_folder(folder_slices_1, folder_slices_2, output_folder, str_description)
 
 if __name__=="__main__":
