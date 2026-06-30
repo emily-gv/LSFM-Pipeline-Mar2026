5cf29114623d9d544a32853bffaf757c52887856
emily-gv <emilygarciavolk@gmail.com>
2026-06-30 17:36:49 -0600

    Modularize

--- correct_cells.py
+++ correct_cells.py
@@ -67,10 +67,45 @@ def matching_label_pairs_perc(matrix1, matrix2, min_perc = 0.5):
 
     return matching_pairs, matching_pairs_non_zero_left, matching_pairs_non_zero
 
-
 from TissueSegmentation.data_loader import get_images_from_path
 
-# using cv2.IMREAD_UNCHANGED to preserve single-channel in label mask thing
+# Where the first path (img_cc3_binary) is the images you want to REMOVE cells from
+def binary_to_label(img_cc3_binary, img_phh3_binary):
+    img_cc3_binary_shifted = cv2.imread(img_cc3_binary, cv2.IMREAD_UNCHANGED)
+    img_phh3_binary = cv2.imread(img_phh3_binary, cv2.IMREAD_UNCHANGED)
+
+    cc3_binaryToLabel, nA = label(img_cc3_binary_shifted)
+    phh3_binaryToLabel, nB = label(img_phh3_binary)
+
+# Where the first path (folder_cc3_binary) is the images you want to REMOVE cells from
+def remove_cells_folder_vs_folder(folder_cc3_binary, folder_phh3_binary, output_folder):
+    list_slices_1 = get_images_from_path(folder_cc3_binary)
+    list_slices_2 = get_images_from_path(folder_phh3_binary)
+    n_list_slices_1 = len(list_slices_1)
+    n_list_slices_2 = len(list_slices_2)
+    if n_list_slices_1 != n_list_slices_2:
+        print('Folders with different number of slices')
+        print('folder 1:' + str(n_list_slices_1))
+        print('folder 2:' + str(n_list_slices_2))
+        return
+
+    for i in range(n_list_slices_1):
+        img_slice_path_1 = list_slices_1[i]
+        img_slice_path_2 = list_slices_2[i]
+
+        cc3_label, phh3_label = binary_to_label(img_slice_path_1, img_slice_path_2)
+        _, _, matching_pairs = matching_label_pairs_perc(cc3_label, phh3_label, min_perc = 0.5)
+
+        img_cc3_filtered = img_cc3_binary_shifted.copy()
+        labels_to_remove = [label1 for label1, _ in matching_pairs]
+        remove_mask = np.isin(cc3_binaryToLabel, labels_to_remove)
+        img_cc3_filtered[remove_mask] = 0
+
+        base = os.path.splitext(os.path.basename(img_slice_path_1))[0]
+        os.makedirs(output_folder, exist_ok=True)
+        output_path = os.path.join(output_folder, base + ".png") 
+        cv2.imwrite(output_path, img_cc3_filtered)
+
 def main():
     # img_phh3_binary = cv2.imread('/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/Step25b_pHH3_slices_binary/Aug2025_27_slice_0500.png', cv2.IMREAD_UNCHANGED)
     # img_cc3_binary = cv2.imread('/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_slices_binary/Aug2025_27_slice_0500.png', cv2.IMREAD_UNCHANGED)
@@ -90,37 +125,6 @@ def main():
     # print("CC3 cell count:", cc3_binaryToLabel.max())
 
     # cv2.imwrite('/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/Aug2025_27_slice_0500_cc3_filtered.png', img_cc3_filtered)
-
-    list_slices_1 = get_images_from_path('/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_slices_binary_shifted')
-    list_slices_2 = get_images_from_path('/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/Step25b_pHH3_slices_binary')
-    n_list_slices_1 = len(list_slices_1)
-    n_list_slices_2 = len(list_slices_2)
-    if n_list_slices_1 != n_list_slices_2:
-        print('Folders with different number of slices')
-        print('folder 1:' + str(n_list_slices_1))
-        print('folder 2:' + str(n_list_slices_2))
-        return
-    for i in range(n_list_slices_1):
-        img_slice_path_1 = list_slices_1[i]
-        img_slice_path_2 = list_slices_2[i]
-        img_cc3_binary_shifted = cv2.imread(img_slice_path_1, cv2.IMREAD_UNCHANGED)
-        img_phh3_binary = cv2.imread(img_slice_path_2, cv2.IMREAD_UNCHANGED)
-
-        cc3_binaryToLabel, nA = label(img_cc3_binary_shifted)
-        phh3_binaryToLabel, nB = label(img_phh3_binary)
-
-        _, _, matching_pairs = matching_label_pairs_perc(cc3_binaryToLabel, phh3_binaryToLabel, min_perc = 0.5)
-
-        img_cc3_filtered = img_cc3_binary_shifted.copy()
-        labels_to_remove = [label1 for label1, _ in matching_pairs]
-        remove_mask = np.isin(cc3_binaryToLabel, labels_to_remove)
-        img_cc3_filtered[remove_mask] = 0
-
-        base = os.path.splitext(os.path.basename(img_slice_path_1))[0]
-        output_folder = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_corrected'
-        os.makedirs(output_folder, exist_ok=True)
-        output_path = os.path.join(output_folder, base + ".png") 
-        cv2.imwrite(output_path, img_cc3_filtered)
 
 if __name__ == "__main__":
     main()
