6ca4edb8bb87af76eda4856d7bff8b4dfe87c4f6
emily-gv <emilygarciavolk@gmail.com>
2026-08-19 15:48:49 -0600

    Folder restructuring

--- createMask.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from aux_functions.CC_Cells import function_encode_annotations
-
-input_folder = r"C:\Users\Emily Garcia-Volk\Documents\EmilyGV_Thesis\CC3_Validation\CC3_Annotations"
-output_folder = r"C:\Users\Emily Garcia-Volk\Documents\EmilyGV_Thesis\CC3_Validation\CC3_Masks"
-
-function_encode_annotations(input_folder, output_folder, flag_binary_mask=False)
