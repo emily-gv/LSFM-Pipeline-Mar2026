4f86cda3ad18b03c01db8cee1577cb936a7f6aa2
emily-gv <emilygarciavolk@gmail.com>
2026-08-19 13:03:24 -0600

    Add documentation

--- compareOverlap.py
+++ compareOverlap.py
@@ -6,6 +6,20 @@ from PIL import Image
 from scipy import ndimage
 
 def compute_image_overlay(image1, image2, output_folder, str_description):
+    """
+    Create a merged image of overlap between CC3 and pHH3. Will only compare between images of the same shape.
+
+    Image 1 = Red
+    Image 2 = Green
+    Overlap = Yellow
+
+    Args:
+        image1 (str) = Full filepath to image slice of cell marker #1
+        image2 (str) = Full filepath to image slice of cell marker #2
+        output_folder (str) = Full filepath where you want the output
+        str_description (str) = Will be added to output image (eg. 'cc3_phh3_comparison')
+    """
+
     mask1 = cv2.imread(image1, cv2.IMREAD_GRAYSCALE) == 255  
     mask2 = cv2.imread(image2, cv2.IMREAD_GRAYSCALE) == 255
     # print("Shape of image1: ", mask1.shape)
@@ -25,6 +39,20 @@ def compute_image_overlay(image1, image2, output_folder, str_description):
         cv2.imwrite(output_path, output)
 
 def compute_image_overlay_folder(folder_slices_1, folder_slices_2, output_folder, str_description):
+        """
+    Iterate through two folders and create merged images of overlap between CC3 and pHH3.
+    Assumed image names are the same between folders because get_images_from_path() returns whatever with sorted()
+
+    folder_slices_1 = Red
+    folder_slices_2 = Green
+    Overlap = Yellow
+
+    Args:
+        folder_slices_1 (str) = Full filepath to folder of cell marker #1
+        folder_slices_2 (str) = Full filepath to folder of cell marker #2
+        output_folder (str) = Full filepath where you want the output
+        str_description (str) = Will be added to output images (eg. 'cc3_phh3_comparison')
+    """
     list_slices_1 = get_images_from_path(folder_slices_1)
     list_slices_2 = get_images_from_path(folder_slices_2)
     n_list_slices_1 = len(list_slices_1)
@@ -45,6 +73,16 @@ def compute_image_overlay_folder(folder_slices_1, folder_slices_2, output_folder
 # Shift left = array[:,:-x]
 # Shift right = array[;,:x]
 def correct_shift(folder_slices_toShift, output_folder, shift_value):
+    """
+    Iterates through folder to correct shift in a hardcoded direction and saves to a new folder.
+    Replace "img_shifted[:-shift_value,:] = img_original[shift_value:, :]" with desired behaviour as outlined in the comments directly above.
+    ** I think I made this to use black pixels and not wrap around but verify that before using **
+
+    Args: 
+        folder_slices_toShift (str): Full filepath
+        output_folder (str): Full filepath
+        shift_value (int): Manually determined pixels to shift by
+    """
     # output = np.zeros_like(image)
     # output[:-shift_value, :] = image[shift_value:, :]
     list_slices = get_images_from_path(folder_slices_toShift)
@@ -65,26 +103,47 @@ def correct_shift(folder_slices_toShift, output_folder, shift_value):
         
         cv2.imwrite(output_path, img_shifted_bw)
 
-    # y, x = np.where(image == 1)
-    # new_y = y - shift_value
-    # valid = new_y >= 0
-    # output[new_y[valid], x[valid]] = image[valid]
+
+def correct_shift_label(folder_slices_toShift, output_folder, shift_value):
+    """
+    Same as correct_shift() but for label images. NO POINT IN USING. 
+    Cellpose labels are kinda bad, correct_cells() script ends up calculating its own labels and using those. 
+    """
+    list_slices = get_images_from_path(folder_slices_toShift)
+    n_list_slices = len(list_slices)
+    for i in range(n_list_slices):
+        img_slice_path = list_slices[i]
+        img_original = cv2.imread(img_slice_path, cv2.IMREAD_UNCHANGED)
+
+        img_shifted = np.zeros_like(img_original)
+        # THE BELOW SHIFTS IMAGE UP
+        img_shifted[:-shift_value,...] = img_original[shift_value:, ...]
+
+        base = os.path.splitext(os.path.basename(img_slice_path))[0]
+        output_path = os.path.join(output_folder, base + '_shifted.png')
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+        
+        cv2.imwrite(output_path, img_shifted)
 
 def main():
     # image1 = '/home/emilygv/Desktop/CC3_pHH3_comparison/Aug28_2025_27/Step25b_pHH3_slices_binary/Aug28_2025_27_slice_0350.png'
     # image2 = '/home/emilygv/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_slices_binary/Aug28_2025_27_slice_0350.png' 
     # compute_image_overlay(image1, image2, output_folder, str_description)
 
-
     # folder_slices_toShift = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_slices_binary'
     # output_folder = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_slices_binary_shifted'
     # correct_shift(folder_slices_toShift, output_folder, 12)
     
-    folder_slices_1 = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/Step25b_pHH3_slices_binary'
-    folder_slices_2 = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_slices_binary_shifted' 
-    output_folder = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/CC3_pHH3_comparison_shifted'
-    str_description = 'phh3_cc3'
-    compute_image_overlay_folder(folder_slices_1, folder_slices_2, output_folder, str_description)
+    # folder_slices_1 = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/Step25b_pHH3_slices_binary'
+    # folder_slices_2 = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_slices_binary_shifted' 
+    # output_folder = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/CC3_pHH3_comparison_shifted'
+    # str_description = 'phh3_cc3'
+    # compute_image_overlay_folder(folder_slices_1, folder_slices_2, output_folder, str_description)
 
+    # folder_slices_toShift = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_slices_label'
+    # output_folder = '/home/emily/Desktop/CC3_pHH3_comparison/Aug28_2025_27/cc3_slices_label_shifted'
+    # correct_shift_label(folder_slices_toShift, output_folder, 12)
+    
 if __name__=="__main__":
     main()
